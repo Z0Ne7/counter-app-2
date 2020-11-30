e7009c00f017072753fe98023fb4f17824ba0ed1
Canh <canhmeoladykiler01@gmail.com>
2020-11-30 15:54:32 +0700

    Done counter with class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onClickPlus = () => {
+    const { count } = this.state;
+    this.setState({
+      count: count + 1,
+    });
+  };
+  onClickMinus = () => {
+    const { count } = this.state;
+    this.setState({
+      count: count - 1,
+    });
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>You clicked {count} times</p>
+        <button onClick={this.onClickMinus}>-</button>
+        <button onClick={this.onClickPlus}>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
